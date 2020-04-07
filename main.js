72d657a8cf63195ea3d0b14516e50ff10e4a0818
Valeria Meza <valeriameza173@gmail.com>
2020-04-07 09:30:02 -0500

    fixed the filter list

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ console.log("Coffee Project");
 
 function renderCoffee(coffee) {
     var html = '<div class="coffee">';
-    // html += '<div>' + coffee.id + '</div>';
     html += '<h2>' + coffee.name + '</h2></div>';
     html += '<div><p>' + coffee.roast + '</p></div>';
     html += '<div>';
@@ -15,7 +14,7 @@ function renderCoffee(coffee) {
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for (var i = coffees.length - 1; i >= 0; i--) {
         coffees.sort(function (a, b) {
             return b.id - a.id
         });
@@ -24,38 +23,41 @@ function renderCoffees(coffees) {
 
     return html;
 }
+
 // Coffee Filter function
 function filterCoffees() {
-    var input, filter, div, h2, a ,i, txtValue;
+    var input, filter, div, h2, para, a, b, i, txtValue;
     input = document.getElementById('search');
     filter = input.value.toLowerCase();
     div = document.getElementById("coffees");
     h2 = div.getElementsByTagName('h2');
+    para = div.getElementsByTagName('p');
 
     for(i = 0; i < h2.length; i++) {
         a = h2[i];
+        b = para;
         txtValue = a.textContent || a.innerText;
+
         if(txtValue.toLowerCase().indexOf(filter) > -1) {
-            h2[i].style.display = "";
+            h2[i].style.display = b[i].style.display = "";
+
         } else {
-            h2[i].style.display = "none";
+            h2[i].style.display = b[i].style.display = "none";
         }
     }
-
-    }
+}
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
+    coffees.forEach(function (coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
-
 
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
